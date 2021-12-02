50c3bae1d741349b7b089779edada3d3a3162c4c
DESKTOP-9BT6J5O\CarlosLoura <cap.loura@gmail.com>
2021-12-02 13:07:35 +0100

    commits

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ const CLIENT_ID =
 const CLIENT_SECRET = "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";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
 const REFRESH_TOKEN =
-  "1//04aerM07xEM_PCgYIARAAGAQSNwF-L9IrM1UrZp_NBXqg2Wg_x3dZh_Ozvobu3o2kU-JNSO8vmt65zAQRUQaqXbk71-EnFuxlamI";
+  "ya29.a0ARrdaM-KUapthX9E8Q6jbViS4hQlsi3LWAxrTG3hydrNS_XXx8s9_Q2Fs5wH0d89t6U0aOor_Mmt45e6EWa7fGj7RjPLlzJyV_TH9UfiXOOu9emivCEdjIRsAL5xIu4lPF7S4yyTv5kv8MxftsY2dLBdgR-B";
 
 const EMAIL = "dentalonboard@gmail.com";
 
